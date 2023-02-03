0b1efb9a02688d649f80589ac81c5fc0fe97c1ff
Andreas Doering <35858164+a-doering@users.noreply.github.com>
2023-02-03 18:13:11 +0100

    Update local lax friedrichs for newer jax versions

--- hj_reachability/artificial_dissipation.py
+++ hj_reachability/artificial_dissipation.py
@@ -23,11 +23,10 @@ def local_lax_friedrichs(partial_max_magnitudes, states, time, values, left_grad
         jnp.maximum(jnp.max(left_grad_values, grid_axes), jnp.max(right_grad_values, grid_axes)))
     local_local_grad_value_boxes = sets.Box(jnp.minimum(left_grad_values, right_grad_values),
                                             jnp.maximum(left_grad_values, right_grad_values))
-    local_grad_value_boxes = jax.tree_multimap(
-        lambda global_grad_value, local_local_grad_values: jax.ops.index_update(
-            jnp.broadcast_to(global_grad_value, values.shape +
-                             (values.ndim,) * 2), jax.ops.index[..., grid_axes, grid_axes], local_local_grad_values),
-        global_grad_value_box, local_local_grad_value_boxes)
+    local_grad_value_boxes = jax.tree_util.tree_map(
+        lambda global_grad_value, local_local_grad_values: jnp.broadcast_to(
+            global_grad_value, values.shape + (values.ndim,) * 2).at[jnp.index_exp[..., grid_axes, grid_axes]].set(local_local_grad_values), global_grad_value_box, local_local_grad_value_boxes
+    )
     return utils.multivmap(
         lambda state, value, grad_value_box: partial_max_magnitudes(state, time, value, grad_value_box),
         grid_axes)(states, values, local_grad_value_boxes)
